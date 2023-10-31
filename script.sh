915ed94ee144ae9f3d5537e3ea77e88d9be8e23c
insightfulbriyan <vesoljec.jk@gmail.com>
2023-10-31 12:59:13 +0100

    Update

---
+++ script.sh
@@ -0,0 +1,6 @@
+pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
+/usr/bin/vendor_perl/biber Komunikacijska\ vezja\ in\ naprave && \
+pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
+/usr/bin/vendor_perl/biber Komunikacijska\ vezja\ in\ naprave && \
+pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
+pdflatex Komunikacijska\ vezja\ in\ naprave.tex
