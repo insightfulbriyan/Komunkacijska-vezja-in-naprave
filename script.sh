3cee543ba7afe1a03c542842a3739b4e85a43371
insightfulbriyan <vesoljec.jk@gmail.com>
2024-04-07 20:01:31 +0200

    Zadnji update pred oddajo

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
+rm -f Komunikacijska\ vezja\ in\ naprave.{aux,bbl,bcf,blg,log,out,run.xml,toc,lof,lot}
 pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
 /usr/bin/vendor_perl/biber Komunikacijska\ vezja\ in\ naprave && \
 pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
 /usr/bin/vendor_perl/biber Komunikacijska\ vezja\ in\ naprave && \
 pdflatex Komunikacijska\ vezja\ in\ naprave.tex && \
 pdflatex Komunikacijska\ vezja\ in\ naprave.tex
+rm -f Komunikacijska\ vezja\ in\ naprave.{aux,bbl,bcf,blg,log,out,run.xml,toc,lof,lot}
